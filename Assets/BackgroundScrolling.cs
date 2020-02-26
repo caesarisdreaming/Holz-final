8c9e989958989f1c6a5e070cda77ce7863fa6fda
Yazid Taki <46725845+Toucano@users.noreply.github.com>
2020-02-26 01:42:07 +0100

    26/02/2020
    Scrolling background in menu

---
+++ Assets/BackgroundScrolling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+
+    public float bgSpeed;
+
+    public Renderer bgRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(0f, bgSpeed * Time.deltaTime);
+    }
+}
