856142ebc928931571df3b877beacc561f94a973
SpicyCatGames <61442059+SilverboltGames@users.noreply.github.com>
2020-11-07 20:31:12 +0600

    Revert "old"
    This reverts commit 1fe299ec5af001b550a299b79d4571760b0c1365.

--- Assets/CommonScripts/BackgroundScrolling.cs
+++ Assets/CommonScripts/BackgroundScrolling.cs
@@ -8,8 +8,13 @@ public class BackgroundScrolling : MonoBehaviour
     public float bgSpeed;
 
     public Renderer bgRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    // Just scrolling the background
+    // Update is called once per frame
     void Update()
     {
         bgRend.material.mainTextureOffset += new Vector2(0f, bgSpeed * Time.deltaTime);
